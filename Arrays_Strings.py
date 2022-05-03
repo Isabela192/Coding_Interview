6dfa68b15ae42782e77720c75a4c6b33e330c432
Isabela Siqueira <isabela7011@gmail.com>
2022-05-02 21:47:52 -0300

    Starting 1.9

--- Arrays_Strings.py
+++ Arrays_Strings.py
@@ -132,6 +132,42 @@ def stringCompression(s):
         return compressed_string
     else:
         return s
-    
-print(stringCompression("aabcccccaaa"))    
+
+
+print(stringCompression("aabcccccaaa"))
 print(stringCompression("abcdef"))
+
+
+# 1.7 Rotate Matrix:
+# Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
+# write a method to rotate the image by 90 degrees. Can you do this in place?
+def rotateMatrix(matrix):
+    n = len(matrix)
+    for layer in range(n//2):
+        first = layer
+        last = n-1-layer
+        for i in range(first, last):
+            offset = i - first
+            top = matrix[first][i]
+            matrix[first][i] = matrix[last-offset][first]
+            matrix[last-offset][first] = matrix[last][last-offset]
+            matrix[last][last-offset] = matrix[i][last]
+            matrix[i][last] = top
+    return matrix
+
+
+print(rotateMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
+
+# 1.8 Zero Matrix:
+# Write an algorithm such that if an element in a MxN matrix is 0, 
+# it's entire row and column are et to 0
+def zeroMatrix(matrix):
+    row = []
+    col = []
+    for i in range(len(matrix[0])):
+        for j in range(len(matrix)):
+            if matrix[i][j] == 0:
+                row = i
+                col = j
+                
